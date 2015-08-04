1cb4989032d42cb3dfc27596ed7f07333baaab5b
Paul Osman <PaulOsman@Pauls-MacBook-Pro.local>
2015-08-04 11:39:35 +0100

    Initial Commit

---
+++ H202-Spray-StopwatchTests/H202_Spray_StopwatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  H202_Spray_StopwatchTests.swift
+//  H202-Spray-StopwatchTests
+//
+//  Created by Paul Osman on 04/08/2015.
+//  Copyright (c) 2015 Paul Osman. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class H202_Spray_StopwatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
